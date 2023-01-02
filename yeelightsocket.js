598372d6a9b8af6c4e09d3e25a5e30653862d125
davidcreager <david.h.creager@btinternet.com>
2023-01-02 20:11:12 +0000

    Initial repo

--- yeelightsocket.js
+++ yeelightsocket.js
@@ -1,92 +1,106 @@
 module.exports = function(RED) {
-	const Denon = require('denon-client');
+	const ws = require("ws");
 	const inspect = require("util").inspect;
-    function denonsockNode(config) {
+    RED.nodes.registerType("yeelightsock",YeelighSockNode);
+	function yeelightsockNode(config) {
 		RED.nodes.createNode(this,config);
-		this.denonClient = null;
+		this.yeeLights = {};
         var node = this;
-		this.sTopic = "Unset";
-		node.status({fill: "yellow", shape: "ring", text: "Initialised "});
-		node.connected = false;
-		this.sendMsg = function(ev, data) {
-			node.send( {topic:node.sTopic, payload: {"event": ev, "data": data} });
-		}
 		node.on('close', ()=> {
-			if (node.denonClient?.disconnect) node.denonClient.disconnect();
-			node.denonClient = null;
-			node.connected = false;
-			node.warn("Closing server ");
+			node.warn("[YeelighSockNode][info] Closing " + Object.values(node.yeeLights).length + " Servers");
+			Object.keys(node.yeeLights).forEach( (wss) => {
+				if (node.yeeLights[wss].server) {
+					node.yeeLights[wss].server.close(3001, "Node-red closing");
+				}
+			});
 		});
         node.on('input', (msg) => {
-			if (msg.payload == "connect" || msg.payload == "disconnect") {
-				node.sTopic = msg.topic;
-				if ( msg.payload == "connect" && (!msg.url) ) {
-					node.error("[denonsock] connect must supply msg.url ");
+			if (msg.payload == "connect" || msg.payload == "disconnect" || msg.payload == "clear" || msg.payload == "status") {
+				if ( msg.payload != "clear" && msg.payload != "status" && (!msg.url) ) {
+					node.error("[websock] connect or disconnect must supply msg.url ");
 					node.status({fill: "red", shape: "ring", text: "Msg.url not valid"});
 					return null;
-				} else if (msg.payload == "disconnect") {
-					if (node.denonClient && (node.connected)) {
-						node.denonClient.disconnect();
-						node.status({fill: "blue", shape: "ring", text: "Disconnected "});
-						node.connected = false;
-					} else {
-						node.status({fill: "yellow", shape: "ring", text: "Disconnected "});
-						node.connected = false;
-					}
+				}
+				if (msg.payload == "status") {
+					node.warn("YeelighSockNode][info][status] " + Object.values(node.yeeLights).length + " Servers");
+					Object.keys(node.yeeLights).forEach( (wss) => {
+						node.warn("YeelighSockNode][info][status] " + wss + " " + node.yeeLights[wss].topic);
+					});
 					return null;
 				}
-				if ( (node.sURL) && (node.sURL != msg.url) ) {
-					//node.wssServer.close();
-				}
-				if (node.denonClient?.disconnect) node.denonClient.disconnect();
-				node.sURL = msg.url;
-				node.denonClient = new Denon.DenonClient(msg.url);
-				node.status({fill: "blue", shape: "ring", text: "connecting to " + node.sURL})
-				node.denonClient.on("masterVolumeChanged", data => node.sendMsg("masterVolumeChanged", data));
-				node.denonClient.on("inputChanged", data => node.sendMsg("inputChanged", data));
-				node.denonClient.on("powerChanged", data => node.sendMsg("powerChanged", data));
-				node.denonClient.on("muteChanged", data => node.sendMsg("muteChanged", data));
-				node.denonClient.connect()
-					.then( () => {
-						node.warn("[denonsock] Connected to " + node.sURL);
-						node.status({fill: "green", shape: "dot", text: "Connected to " + node.sURL});
-						node.connected = true;
-					})
-					.then( () => {
-						node.denonClient.getPower();
-					})
-					.catch( (err) => {
-						node.error("[denonsock] Connection failed with " + err);
-						node.status({fill: "red", shape: "ring", text: "Failed " + err})
-						node.connected = false
+				if (msg.payload == "clear") {
+					node.warn("[YeelighSockNode][info][clear] Clearing " + Object.values(node.yeeLights).length + " Servers");
+					Object.keys(node.yeeLights).forEach( (wss) => {
+						node.warn("[YeelighSockNode][info][clear] Closing " + node.yeeLights[wss].url);
+						node.yeeLights[wss].server.close(3002, "User Cleared servers");
+						node.yeeLights[wss] = {}; 
 					});
-			} else if ( typeof(msg.payload) == "object" &&
-							msg.payload.hasOwnProperty("command") ) {
-					if (!node.connected) {
-						node.error("[denonsock][Error] Command received and socket not connected");
-						node.status({fill: "yellow", shape: "ring", text: "Disconnected "});
+					node.status({fill: "yellow", shape: "ring", text: "Cleared " + msg.url})
+					return null;
+				}
+				if (msg.payload == "disconnect") {
+					if (!node.yeeLights[msg.url]) {
+						node.warn("[YeelighSockNode][info][disconnect] Server " + msg.url + " Not opened ");
 						return null;
 					}
-					const cmds = {setInput: true, setMute: true, setPower: true, setVolume:true, getPower: true, getVolume: true}
-					if (cmds[msg.payload.command]) {
-						node.denonClient[msg.payload.command](msg.payload.value)
-						.then( (ret) => {
-							//node.status({fill: "green", shape: "dot", text: msg.payload.command + " sent to " + node.sURL});
-							node.status({fill: "red", shape: "dot", text: msg.payload.command + " ret= " + ret});
-						})
-						.catch( (err) => {
-							node.status({fill: "red", shape: "ring", text: msg.payload.command + "Failed " + err})
-						});
+					if (!node.yeeLights[msg.url].server) {
+						node.warn("[YeelighSockNode][debug][disconnect] Closing Server " + msg.url);
+						node.yeeLights[msg.url].server.close(3003, "User Disconnected Server");
+						node.status({fill: "blue", shape: "ring", text: "disconnecting "});
 					} else {
-						node.error("[denonsock] Payload command must be " + Object.keys(cmds).join("|") + " it is " + msg.payload);
-						node.status({fill: "red", shape: "ring", text: "Unknown Command " + msg.payload.command});
+						node.warn("[YeelighSockNode][debug][disconnect]  Server already closed " + msg.url);
+						node.status({fill: "yellow", shape: "ring", text: "Disconnected "});
 					}
+					node.yeeLights[msg.url] = null;
+					return null;
+				}
+				if (node.yeeLights[msg.url]) {
+					node.warn("[YeelighSockNode][info] Server " + msg.url + " already connected will close and reconnect");
+					node.yeeLights[msg.url].server.close(3004, "Re-opening Server");
+					node.warn("[YeelighSockNode][debug][connect]  Re Opening " + msg.url);
+				} else {
+					node.warn("[YeelighSockNode][debug][connect]  Opening " + msg.url);
+					node.yeeLights[msg.url] = {};
+				}
+				node.yeeLights[msg.url].server = new ws.WebSocket(msg.url);
+				node.yeeLights[msg.url].topic = msg.topic;
+				node.yeeLights[msg.url].url = msg.url;
+				node.yeeLights[msg.url].onMessage = function(url, data) {
+						let tdata = null;
+						try {
+							tdata = JSON.parse(data);
+						} catch (err) {
+							tdata = data + "";
+							node.warn(["mesg NOT parsed " + err, err, tdata, data,msg]);
+						}
+						node.send({topic: node.yeeLights[url].topic, url: node.yeeLights[url].url, payload: tdata});
+					};
+				node.yeeLights[msg.url].onClose = function(url, ev) {
+						node.warn(["Close Event Received url=" + url, ev]);
+						node.send({topic: node.yeeLights[url].topic + "/close", url: node.yeeLights[url].url, payload: ev});
+					};
+				node.status({fill: "blue", shape: "ring", text: "connecting to " + msg.url})
+				node.yeeLights[msg.url].server.on("open", () => {
+					node.status({fill: "green", shape: "dot", text: "connected:" + msg.url});
+				});
+				node.yeeLights[msg.url].server.on("error", data => {
+					node.status({fill: "red", shape: "ring", text: "error:" + msg.url + " " + data});
+					
+				});
+				node.yeeLights[msg.url].server.on("close", (ev) => {
+					node.status({fill: "yellow", shape: "ring", text: "closed:"  + msg.url + " " + ev.reason});
+					node.yeeLights[msg.url].onClose(msg.url, ev);
+				});
+				node.yeeLights[msg.url].server.on("message", data => {
+					node.yeeLights[msg.url].onMessage(msg.url, data);
+				});
+				return null;
 			} else {
-				node.error("[denonsock] Payload must be connect|disconnect it is " + msg.payload);
+				node.error("[websock] Payload must be connect|disconnect||clear was  " + msg.payload);
 				node.status({fill: "red", shape: "ring", text: "Unknown payload " + msg.payload});
 				return null;
 			}
         });
     }
-    RED.nodes.registerType("denonsock",denonsockNode);
+    RED.nodes.registerType("yeelightsock",YeelighSockNode);
 }
